369e4ccc118d9eee234e46eda904da3933901ae1
Margot Wagner <mwagner92796@gmail.com>
2020-05-20 18:23:04 -0700

    fixed to point to slurm script

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 dirs = sorted(glob.glob("psweep_num_*/dist_*"))
 
 for d in dirs:
-	cmd = '(cd ./%s/ ; pbs_run_mcell_range.py "1:1000" 16 Scene.main.mdl)' % d
+	cmd = '(cd ./%s/ ; slurm_run_mcell_range.py "1:1000" 16 Scene.main.mdl)' % d
 	print(cmd)
 	os.system(cmd)
 
