b9755b991d338c1db382dd8446bafac9ce3ab0bb
Margot Wagner <mwagner92796@gmail.com>
2020-05-20 18:14:46 -0700

    submitting jobs python script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+dirs = sorted(glob.glob("psweep_num_*/dist_*"))
+
+for d in dirs:
+	cmd = '(cd ./%s/ ; pbs_run_mcell_range.py "1:1000" 16 Scene.main.mdl)' % d
+	print(cmd)
+	os.system(cmd)
+
